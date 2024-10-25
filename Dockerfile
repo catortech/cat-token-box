19c83c0d47cc8972f7df176289a07367aa513804
Orlando Fiol <orlando.fiol@trafilea.com>
2024-10-24 22:55:54 -0400

    Fix: Run worker and api. Add Datasource dep to mempool service

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,5 @@ WORKDIR /app/packages/tracker
 RUN yarn install && yarn build
 
 EXPOSE 3000
-CMD ["yarn", "start:prod"]
+CMD ["yarn", "start:worker:prod"]
+CMD ["yarn", "start:api:prod"]
--- packages/tracker/src/services/mempool/mempool.module.ts
+++ packages/tracker/src/services/mempool/mempool.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { MempoolService } from './mempool.service';
 import { RpcModule } from '../rpc/rpc.module';
 import { ConfigModule } from '@nestjs/config';
-import { TxModule } from '../tx/tx.module';
 import { CommonModule } from '../common/common.module';
+import { ormConfig } from 'src/config/db.config';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [RpcModule, ConfigModule, CommonModule],
-  providers: [MempoolService],
+  providers: [
+    MempoolService,
+    {
+      provide: DataSource,
+      useFactory: async () => {
+        const dataSource = new DataSource(ormConfig);
+        return await dataSource.initialize();
+      },
+    },
+  ],
   exports: [MempoolService],
 })
 export class MempoolModule {}
